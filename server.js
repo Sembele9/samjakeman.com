4a5803952dff0b5ef8e4149e0dae6685c38ac66e
Sam Jakeman <sam.jakeman+sgltd@supergroup.com>
2026-02-19 15:13:11 +0000

    Implement Instagram reel downloader with Puppeteer and fix server deployment

--- server.js
+++ server.js
@@ -12,7 +12,15 @@ async function getBrowser() {
   if (!browser) {
     browser = await puppeteer.launch({
       headless: true,
-      args: ['--no-sandbox', '--disable-setuid-sandbox']
+      args: [
+        '--no-sandbox',
+        '--disable-setuid-sandbox',
+        '--disable-dev-shm-usage',
+        '--disable-accelerated-2d-canvas',
+        '--no-first-run',
+        '--no-zygote',
+        '--disable-gpu'
+      ]
     });
   }
   return browser;
